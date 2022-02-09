3d7e3f64ebb7b69c81f425a5d00296368ca04f4d
Dídac Ballabriga <didac@ballabriga.com>
2022-02-09 11:01:15 +0100

    exercici7

--- js/shop.js
+++ js/shop.js
@@ -64,7 +64,7 @@ var cart = [];
 var total = 0;
 
 // Exercise 1
-function buy(id) {
+/*function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 for (let i = 0 ; i < products.length; i++) {  
@@ -74,26 +74,26 @@ for (let i = 0 ; i < products.length; i++) {
     }
 }
  // console.log(cartList);
-}
+}*/
 
 // Exercise 2
 function cleanCart() {
- cartList.length = 0;
+ cart.length = 0;
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
     var tot = 0;
-    for (let i = 0; i < cartList.length; i++){
-        var total = cartList[i].price;
+    for (let i = 0; i < cart.length; i++){
+        var total = cart[i].subtotal;
         var tot = total + tot;
     }
-   console.log(tot);
+   console.log(tot.toFixed(2));
 }
 
 // Exercise 4
-function generateCart() {
+/*function generateCart() {
 // Using the "cartlist" array that contains all the items in the shopping cart, 
 // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product. 
     for (let i =0; i<cartList.length; i++){
@@ -103,19 +103,26 @@ function generateCart() {
         // console.log("findDuplicate: " + findDuplicate);
         if (findDuplicate == -1){
             // console.log("NO EXISTE");
-            var cartItem = {id: cartList[i].id, name: cartList[i].name, price:cartList[i].price, type: cartList[i].type, quantity: 1};    
+            var cartItem = {name: cartList[i].name, price:cartList[i].price, type: cartList[i].type, quantity: 1, subtotal:0, subtotalWithDiscount:0};    
             cart.push(cartItem);      
         } else {
             // console.log("EXISTE");
-            cart[findDuplicate].quantity++;              
+            cart[findDuplicate].quantity++;     
         }  
     }    
-    console.log(cart);   
-}
+}*/
 
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    for (let i = 0; i<cart.length; i++){
+        if (cart[i].name == 'cooking oil' && cart[i].quantity >= 3){
+        cart[i].price=10;
+         } else if (cart[i].name == 'Instant cupcake mixture' && cart[i].quantity >= 10){
+        cart[i].price = cart[i].price*0.6; 
+        }
+    }
+       console.log(cart)
 }
 
 
@@ -126,7 +133,25 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    for (let i = 0 ; i<products.length; i++){
+          if(id-1 == i){
+              //console.log("buscamos duplicado");
+              var findDuplicate = cart.findIndex(dupli => dupli.name == products[i].name);
+              if(findDuplicate > -1){
+                 // console.log("Existe, suma cantidad y precio");
+                    cart[findDuplicate].quantity++;
+                    cart[findDuplicate].subtotal += cart[findDuplicate].price;
+              } else {
+                //  console.log("no existe, crear producto");
+                  var cartItem = {name: products[i].name, price:products[i].price, type: products[i].type, quantity: 1, subtotal:products[i].price, subtotalWithDiscount:0};  
+                  cart.push(cartItem);  
+              }
+            }
+    }  
 }
+
+
+
 
 // Exercise 8
 function removeFromCart(id) {
