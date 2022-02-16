caebb39f2748a9e12278ee97dcdeb651e50448be
Dídac Ballabriga <didac@ballabriga.com>
2022-02-16 15:36:23 +0100

    fix conflicts again4

--- js/shop.js
+++ js/shop.js
@@ -63,7 +63,7 @@ var cart = [];
 
 var total = 0;
 
-//Exercise 1
+/* Exercise 1
 function buy(id) {  
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
@@ -76,7 +76,7 @@ function buy(id) {
   console.log("cartList:");
   console.log(cartList);
   generateCart()
-}
+}*/
 
 // Exercise 2
 function cleanCart() {
